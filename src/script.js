869a9a09462baaed38dff7425c31ecd8bf58dd00
cailaar <caila.alyssa.ryner@gmail.com>
2023-02-04 03:24:31 +0000

    Initial Website Design

---
+++ src/script.js
@@ -0,0 +1,44 @@
+// //Slideshow 
+
+let myTimeout;
+
+function increaseSlide() {
+  clearTimeout(myTimeout);
+  showSlides();
+}
+
+function decreaseSlide() {
+  clearTimeout(myTimeout);
+  slideIndex-=2
+  showSlides();
+}
+
+function displaySlide(n){
+  clearTimeout(myTimeout)
+  slideIndex = n-1;
+  showSlides();
+}
+
+
+let slideIndex = -1;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  slideIndex++;
+  if (slideIndex < 0){ slideIndex = slides.length-1}
+  if (slideIndex > slides.length-1) {slideIndex = 0}    
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex].style.display = "block";  
+  dots[slideIndex].className += " active";
+  myTimeout = setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
